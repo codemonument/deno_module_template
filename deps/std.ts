1d97b63c7271ef565aa565914cc794248ccf8a8a
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 11:36:36 +0200

    move deps folder into source folder

--- deps/std.ts
+++
@@ -1,1 +0,0 @@
-export * as log from "https://deno.land/std@0.180.0/log/mod.ts";
--- src/dummy.ts
+++ src/dummy.ts
@@ -1,2 +1,6 @@
+import { log } from "./deps/std.ts";
+
 export const dummyExport =
   "This is a dummy export. Please remove for production!";
+
+log.warning(dummyExport);
