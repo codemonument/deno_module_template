b77c359b5e2fcc09bf5fc0e04190d940f95e44f6
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 11:18:29 +0200

    get example folder it's own deno.jsonc config

--- example/main.ts
+++ example/main.ts
@@ -1,8 +1,10 @@
-import { } from "@/mod.ts";
-import { log } from "@/deps/std.ts";
+import {} from "@mod";
+import * as log from "$std/log/mod.ts";
 
 try {
-  log.info(`Your module is currently empty! Add some code to the lib folder and export it in mod.ts!`);
+  log.info(
+    `Your module is currently empty! Add some code to the lib folder and export it in mod.ts!`,
+  );
 } catch (error) {
   console.error(error);
   Deno.exit();
