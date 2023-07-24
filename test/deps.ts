85e04389debbccb6354c04af07fe8dd9ccae8431
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 11:37:04 +0200

    simplify test dir by having a simple "deps.ts file"

---
+++ test/deps.ts
@@ -0,0 +1,9 @@
+/**
+ * Dependencies ONLY for tests!
+ * More Test dependencies are in import_map.json
+ */
+
+// eslint-disable-next-line deno-warn
+export * from "$std/testing/bdd.ts";
+export * from "$std/testing/asserts.ts";
+export * from "$std/testing/snapshot.ts";
